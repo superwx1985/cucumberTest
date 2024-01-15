7ed02353e52e07a09ff75c60a98ce262fab00d0e
Vic <superwx1985@gmail.com>
2024-01-15 12:13:05 +0800

    modify the structure of project

--- src/test/java/vic/test/runner/ApiRunner.java
+++ src/test/java/vic/test/runner/ApiRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features = "test/resources/features/apiTest.feature", glue = "vic.test.step", monochrome = true)
+@CucumberOptions(features = "src/test/resources/features/apiTest.feature", glue = "vic.test.step", monochrome = true)
 public class ApiRunner extends AbstractTestNGCucumberTests {
 
 }
--- src/test/java/vic/test/runner/UiRunner.java
+++ src/test/java/vic/test/runner/UiRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features = {"test/resources/features/uiTest.feature",},
+@CucumberOptions(features = {"src/test/resources/features/uiTest.feature",},
         glue = "vic.test.step", monochrome = true)
 public class UiRunner extends AbstractTestNGCucumberTests {
 
